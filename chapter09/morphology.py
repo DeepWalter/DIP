c69b4955d4d701780bb296d62a3ee3bd3511db44
JiaXiao <xiao.jia@yizhun-ai.com>
2019-07-02 17:41:04 +0800

    Add morphology.py

---
+++ chapter09/morphology.py
@@ -0,0 +1,13 @@
+class StructuringElement:
+    """Structuring elements."""
+
+    def __init__(self, data, origin=None):
+        pass
+
+    def indices(self):
+        """Return the indices of the elements in this SE centered at origin."""
+        pass
+
+    def show(self):
+        """Plot the structuring element."""
+        pass
