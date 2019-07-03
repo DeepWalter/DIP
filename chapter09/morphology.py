17df4fde58622a3b4dc2ed6419f9143c11ad40a1
JiaXiao <xiao.jia@yizhun-ai.com>
2019-07-03 14:51:06 +0800

    Add conversion between binary images and sets.

--- chapter09/morphology.py
+++ chapter09/morphology.py
@@ -1,91 +1,79 @@
 import numpy as np
 
 
-class StructuringElement:
-    """Structuring element.
+def bimage2set(image, origin=None):
+    """Convert a binary image into a set of coordinates.
+
+    Only the coordinates of those non-zero pixels are collected. And
+    their coordinates are computed w.r.t. the given origin. That is, if
+    a non-zero pixel has indices `(i, j)` in the image, then its
+    coordinates w.r.t. origin is `(i, j) - origin` where `origin` is
+    the numeric form of origin.
 
     Parameters
     ----------
-    data : np.ndarray
-        Image representation or coordinates representation of the
-        structuring element.
-    origin : (int, int), optional
-        Coordinates of the origin.
-    coordinates : bool, optional
-        Whether the data is an image representation or coordinates
-        representation of the structuring element.
-
-    Attributes
-    ----------
-    origin : (int, int)
-        Coordinates of the origin.
     image : np.ndarray
-        Image representation of this structuring element.
-    elems : np.ndarray of shape (*, 2)
-        Coordinates representation of this structuring element.
+        Input binary image.
+    origin : (int, int), optional
+        Coordinates of the origin (default to None, which means (0, 0)).
+
+    Returns
+    -------
+    np.ndarray
+        The set of all non-zero pixels' coordinates, stored as a ndarray
+        of shape (*, 2).
+
+    See Also
+    --------
+    set2bimage: convert a set of coordinates to an image.
+
+    Note
+    ----
+    May add support for collecting zero pixels.
     """
+    # TODO: Add more options for origin, e.g. 'center'
+    if origin is None:
+        origin = (0, 0)
 
-    def __init__(self, data, origin=None, coordinates=False):
-        if coordinates:
-            self.elems = data
-        else:
-            self._origin = origin
-            self.image = data
-
-    @property
-    def image(self):
-        return self._image
-
-    @image.setter
-    def image(self, data):
-        self._image = data
-        self._elems = np.transpose(np.nonzero(data)) - self._origin
-
-    @property
-    def elems(self):
-        return self._elems
-
-    @elems.setter
-    def elems(self, data):
-        self._elems = data
-        xmin, xmax = np.amin(data[:, 1]), np.amax(data[:, 1])
-        ymin, ymax = np.amin(data[:, 0]), np.amax(data[:, 0])
-        print(xmin, xmax)
-        self._origin = (-ymin, -xmin)
-        self._image = np.zeros((xmax-xmin+1, ymax-ymin+1), dtype=np.uint8)
-        x = (data + self._origin)[:, 0]
-        y = (data + self._origin)[:, 1]
-        self._image[x, y] = 1
-
-    def to_image(self):
-        """Convert the structuring element into an image."""
-        pass
-
-    def show(self):
-        """Plot the structuring element."""
-        pass
-
-
-def bimage2set(image, origin=None):
-    """Convert a binary image to the set of coordinates."""
-    pass
+    return np.transpose(np.nonzero(image)) - origin
 
 
 def set2bimage(set):
-    pass
+    """Convert a set of coordinates into an image.
+
+    This is the inverse function of `bimage2set()`. The resulting image
+    is the smallest rectangular image in which all coordinates in the
+    set fit according to their relative positions. The coordinates
+    correspond to the non-zero pixels in the resulting image. The
+    indices of the origin is also returned so that the original set of
+    coordinates can be recovered from them and the resulting image.
+
+    Parameters
+    ----------
+    set : np.ndarray
+        The set of coordinates with shape (*, 2).
+
+    Returns
+    -------
+    image : np.ndarray
+        The corresponding image.
+    origin : (int, int)
+        Indices of the origin in the resulting image.
+
+    See Also
+    --------
+    bimage2set : Inverse function.
+    """
+    ymin, xmin = np.amin(set, axis=0)
+    ymax, xmax = np.amax(set, axis=0)
+
+    origin = (-ymin, -xmin)
+    image = np.zeros((ymax - ymin + 1, xmax - xmin + 1), dtype=np.uint8)
+    x, y = np.hsplit(set + origin, 2)
+    image[x, y] = 1
+
+    return image, origin
 
 
 if __name__ == '__main__':
-    # data = np.zeros((3, 3), dtype=np.uint8)
-    # data[:, 1] = 1
-    # data[1, :] = 1
-    # print(data)
-
-    data = np.array([[-1, 0],
-                     [0, -1],
-                     [0, 0],
-                     [1, 0],
-                     [0, 1]])
-
-    mask = StructuringElement(data, origin=(1, 1), coordinates=True)
-    print(mask.image)
+    pass
